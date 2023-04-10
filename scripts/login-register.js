628b4d389c1035f88bb1b575ba93d17796ec6f26
Devorius-Nagara <adrian.kotiv@gmail.com>
2023-04-10 17:47:25 +0300

    Dark theme in register form, has been fixed

--- scripts/login-register.js
+++ scripts/login-register.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
         changeTheme()
     }
 });
-
 function changeTheme() {
     let themedata;
     if (css.href.match('styles/login-register-white.css')) {
